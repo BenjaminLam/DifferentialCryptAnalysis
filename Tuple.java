cbc01617427f3c7cc21b65664542091649b781b1
BenjaminLam <benjaminlam@me.com>
2018-03-05 16:39:15 +0100

    Tuples updated for cipherthree

--- Tuple.java
+++ Tuple.java
@@ -1,28 +1,49 @@
 public class Tuple {
-    private int x;
-    private int y;
-    public Tuple(int x, int y){
-        this.x=x;
-        this.y=y;
+    private int m0;
+    private int m1;
+    private int c0;
+    private int c1;
+    public Tuple(int m0, int c0, int m1, int c1){
+        this.m0=m0;
+        this.m1=m1;
+        this.c0=c0;
+        this.c1=c1;
     }
 
-    public int getX() {
-        return x;
+    public int getC0() {
+        return c0;
     }
 
-    public int getY() {
-        return y;
+    public int getC1() {
+        return c1;
     }
 
-    public void setX(int x) {
-        this.x = x;
+    public int getM0() {
+        return m0;
     }
 
-    public void setY(int y) {
-        this.y = y;
+    public int getM1() {
+        return m1;
     }
 
-    public boolean equals(Tuple obj) {
-        return (this.x==obj.getX() && this.y==obj.getY());
+    public void setC0(int c0) {
+        this.c0 = c0;
+    }
+
+    public void setM0(int m0) {
+        this.m0 = m0;
+    }
+
+    public void setM1(int m1) {
+        this.m1 = m1;
+    }
+
+    public void setC1(int c1) {
+        this.c1 = c1;
+    }
+
+    @Override
+    public String toString() {
+        return "((" + this.m0 + "," + this.c0 + "),(" + this.m1 + "," + this.c1 +"))";
     }
 }
